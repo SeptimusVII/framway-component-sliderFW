57be053f84704dd455b769f53c7e583047bf5ca7
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-07-26 18:17:53 +0200

    clean and update sample file

--- sliderFW.js
+++ sliderFW.js
@@ -3,8 +3,8 @@ module.exports = function(app){
     var SliderFW = Object.getPrototypeOf(app).SliderFW = new app.Component("sliderFW");
     // SliderFW.debug = true;
     SliderFW.createdAt      = "2.0.0";
-    SliderFW.lastUpdate     = "2.0.2";
-    SliderFW.version        = "1.0.1";
+    SliderFW.lastUpdate     = "2.4.0";
+    SliderFW.version        = "1.0.2";
     // SliderFW.factoryExclude = true;
     // SliderFW.loadingMsg     = "This message will display in the console when component will be loaded.";
     // SliderFW.requires       = [];
