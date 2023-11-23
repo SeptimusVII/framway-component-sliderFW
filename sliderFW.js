559274702da1f7f477c6226ce2e90fa42a7b323c
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-11-23 17:21:44 +0100

    update component to 2.4.3 new standards on img-container

--- sliderFW.js
+++ sliderFW.js
@@ -3,8 +3,8 @@ module.exports = function(app){
     var SliderFW = Object.getPrototypeOf(app).SliderFW = new app.Component("sliderFW");
     // SliderFW.debug = true;
     SliderFW.createdAt      = "2.0.0";
-    SliderFW.lastUpdate     = "2.4.1";
-    SliderFW.version        = "1.0.3";
+    SliderFW.lastUpdate     = "2.4.3";
+    SliderFW.version        = "1.0.4";
     // SliderFW.factoryExclude = true;
     // SliderFW.loadingMsg     = "This message will display in the console when component will be loaded.";
     // SliderFW.requires       = [];
